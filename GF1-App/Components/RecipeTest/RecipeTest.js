88a41af8e325382965485618fc06bd5c956db6c6
Lucas Micoud <lucas.micoud@protonmail.com>
2023-01-29 19:18:20 +0100

    code indent

--- GF1-App/Components/RecipeTest/RecipeTest.js
+++ GF1-App/Components/RecipeTest/RecipeTest.js
@@ -1,4 +1,3 @@
-RecipeTest
 import React, { useState } from 'react';
 import {View, Button, Text, ScrollView } from 'react-native';
 import styles from '../../styles';
@@ -10,70 +9,44 @@ import AddToList from './AddToListButton';
 let countries=[{id:1,name:'France'},{id:2,name:'Europe'},{id:3,name:'Afrique'},{id:4,name:'Amérique'},{id:5,name:'Asie'}];
 
 const RecipeTest=({icon}) => {
-    const [selectedItem,setSelectedItem]=useState(null);
-    const onSelect=(item)=>{
-        setSelectedItem(item);
+  const [selectedItem,setSelectedItem]=useState(null);
+  const onSelect=(item)=>{
+    setSelectedItem(item);
+  }
+  const [selectedItem2,setSelectedItem2]=useState(null);
+  const onSelect2=(item)=>{
+    setSelectedItem2(item);
+  }
+  return (
 
-    }
-    const [selectedItem2,setSelectedItem2]=useState(null);
-    const onSelect2=(item)=>{
-        setSelectedItem2(item);
-
-    
-    }
-    
-    
-    
-
-    return (
+    <View>
+      <SvgLogoIcon/>
+      <AddToList/>
+      <ScrollView >
+        <View style={styles.home.container}>
         
-            <>
-            <SvgLogoIcon/>
-            
-            <AddToList/>
-            
-            <ScrollView >
-           
-            
+          <Text>Etapes de la Recette</Text>
+          <Text style={styles.recipe.text}>1)Couper les Oignons</Text>
+          <Text style={styles.recipe.text}>2)Faire revenir les pommes</Text>
+          <Text style={styles.recipe.text} >Ah ! non ! c’est un peu court, jeune homme !</Text>
 
-            
-            <View style={styles.home.container}>
-                
-            <Text>Etapes de la Recette</Text>
-            <Text style={styles.recipe.text}>1)Couper les Oignons</Text>
-            <Text style={styles.recipe.text}>2)Faire revenir les pommes</Text>
-            <Text style={styles.recipe.text} >Ah ! non ! c’est un peu court, jeune homme !</Text>
-
-            <Text style={styles.recipe.text}>On pouvait dire, oh bien des choses en somme!</Text>
-            </View>
-            
-            <Text style={styles.recipe.text}>Provenance des ingrédients</Text>
-            
-            <Text style={styles.recipe.text}>Ingrédient 1</Text>
-            <Dropdown 
-            data={countries}
-            value={selectedItem}
-            onSelect={onSelect}
-            />
-            <Text style={styles.recipe.text}>Ingrédient 2</Text>
-            <Dropdown 
-            data={countries}
-            value={selectedItem2}
-            onSelect={onSelect2}
-            />
-            </ScrollView>
-            
-
-
-            
-            
-            
-        </>
-            
-            
-        
-    )
+          <Text style={styles.recipe.text}>On pouvait dire, oh bien des choses en somme!</Text>
+        </View>
+        <Text style={styles.recipe.text}>Provenance des ingrédients</Text>
+        <Text style={styles.recipe.text}>Ingrédient 1</Text>
+        <Dropdown 
+          data={countries}
+          value={selectedItem}
+          onSelect={onSelect}
+          />
+        <Text style={styles.recipe.text}>Ingrédient 2</Text>
+        <Dropdown 
+          data={countries}
+          value={selectedItem2}
+          onSelect={onSelect2}
+        />
+      </ScrollView>        
+    </View>
+  )
 }
-
-
 export default RecipeTest;
