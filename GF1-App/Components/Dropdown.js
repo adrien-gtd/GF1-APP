c89aff9a9300a414ca1a1eb4a569e02bd9dc6bcd
Lucas Micoud <lucas.micoud@protonmail.com>
2023-01-31 10:32:39 +0100

    fixed bugged in style reference in dropdown.js

--- GF1-App/Components/Dropdown.js
+++ GF1-App/Components/Dropdown.js
@@ -6,46 +6,43 @@ import styles from '../styles';
 
 
 
-const Dropdown=({data=[]
-    ,value={},
-    onSelect=  ()=>{}
-})=>{
-    console.log("selected value",value);
-    const [showOption,setShowOption]=useState(false);
-    const onSelectedItem= (val)=>{
-        setShowOption(false);
-        onSelect(val);
-    }
-    return(
-        <View>
-            <TouchableOpacity 
-            style={styles.recipe.dropDown} 
-            activeOpacity={0.8}
-            onPress={()=> setShowOption(!showOption)}>
-                <Text>{!!value? value.name: 'Choisir une provenance'}</Text>
-                
-            </TouchableOpacity>
-            {showOption && (<View style={{backgroundColor:COLORS.dropwdownBackgroundColor,
+const Dropdown=({
+  data=[],
+  value={},
+  onSelect = ()=>{}
+}) =>{
+  console.log("selected value",value);
+  const [showOption,setShowOption]=useState(false);
+  const onSelectedItem= (val) => {
+    setShowOption(false);
+    onSelect(val);
+  }
+  return(
+    <View>
+      <TouchableOpacity 
+      style={styles.recipeFull.dropDown} 
+      activeOpacity={0.8}
+      onPress={()=> setShowOption(!showOption)}>
+      <Text> {!!value? value.name: 'Choisir une provenance'} </Text>
+      </TouchableOpacity>
+        {showOption && (<View style={{backgroundColor:COLORS.dropwdownBackgroundColor,
         padding:5,
         marginBottom:10,
         paddingHorizontal:5,
-        }}>
-                {data.map((val,i)=>{
-                return(
-                <TouchableOpacity
-                 key={String(i)}
-                 onPress={()=>onSelectedItem(val)}
-                 >
-                <View style={styles.recipe.itemsDropdown}>
-                        <Text>{val.name}</Text> 
-                </View>
-                        
-                   </TouchableOpacity>
-                )
-            })}</View>)}
-            
-        </View>
-    )
+    }}>
+            {data.map((val,i)=>{
+            return(
+            <TouchableOpacity
+              key={String(i)}
+              onPress={ () => onSelectedItem(val) }>
+              <View style={styles.recipeFull.itemsDropdown}>
+              <Text>{val.name}</Text> 
+            </View>   
+          </TouchableOpacity>
+        )
+      })}</View>)}
+    </View>
+  )
 }
 
 export default Dropdown;
