e2b505562b3c3e35b770806972a483f77cbd6ad0
Lucas Micoud <lucas.micoud@protonmail.com>
2023-04-03 10:36:09 +0200

    removed lorem ipsum from recipe description

--- GF1-App/Components/RecipeFull/RecipeDescription.js
+++ GF1-App/Components/RecipeFull/RecipeDescription.js
@@ -5,7 +5,6 @@ import { CONFIG } from '../../config';
 import styles from '../../styles'
 
 const RecipeDescription = ({ description, isModalVisible, setModalVisibility }) => {
-  description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.|Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.|Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.|Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.|Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   description = description.split(CONFIG.recipeDescriptionSeparator);
 
   return (
