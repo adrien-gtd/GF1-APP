146015123de3ce00524a00636e8f322754a2c328
Raphaël Le Blanc <27leblancraphael@gmail.com>
2023-05-25 16:08:29 +0200

    scrapping poid moyen et prix pas ajustés

---
+++ GF1-App/Components/SVGSettingsComponent.js
@@ -0,0 +1,159 @@
+import * as React from "react"
+
+const SvgSettingsIcon = (props) => (
+  <Svg
+    width={294.803}
+    height="78mm"
+    viewBox="0 0 78 78"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g transform="translate(-54 -60)">
+      <circle
+        style={{
+          fill: "none",
+          stroke: "#000",
+          strokeWidth: 9.85048,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        cx={93}
+        cy={99}
+        r={25.075}
+      />
+      <rect
+        style={{
+          fill: "none",
+          stroke: "#000",
+          strokeWidth: 8.69263,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        width={9.307}
+        height={4.307}
+        x={58.346}
+        y={97.346}
+        ry={0.196}
+      />
+      <rect
+        style={{
+          fill: "none",
+          stroke: "#000",
+          strokeWidth: 8.69263,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        width={9.307}
+        height={4.307}
+        x={118.346}
+        y={97.346}
+        ry={0.196}
+      />
+      <rect
+        style={{
+          fill: "none",
+          stroke: "#000",
+          strokeWidth: 8.69263,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        width={9.307}
+        height={4.307}
+        x={64.346}
+        y={-95.154}
+        ry={0.196}
+        transform="rotate(90)"
+      />
+      <rect
+        style={{
+          fill: "none",
+          stroke: "#000",
+          strokeWidth: 8.69263,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        width={9.307}
+        height={4.307}
+        x={124.346}
+        y={-95.154}
+        ry={0.196}
+        transform="rotate(90)"
+      />
+      <circle
+        style={{
+          fill: "none",
+          stroke: "#000",
+          strokeWidth: 9.85048,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        cx={136.283}
+        cy={3.546}
+        transform="rotate(45.188)"
+        r={25.075}
+      />
+      <rect
+        style={{
+          fill: "none",
+          stroke: "#000",
+          strokeWidth: 8.69263,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        width={9.307}
+        height={4.307}
+        x={101.63}
+        y={1.892}
+        ry={0.196}
+        transform="rotate(45.188)"
+      />
+      <rect
+        style={{
+          fill: "none",
+          stroke: "#000",
+          strokeWidth: 8.69263,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        width={9.307}
+        height={4.307}
+        x={161.63}
+        y={1.892}
+        ry={0.196}
+        transform="rotate(45.188)"
+      />
+      <rect
+        style={{
+          fill: "none",
+          stroke: "#000",
+          strokeWidth: 8.69263,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        width={9.307}
+        height={4.307}
+        x={-31.108}
+        y={-138.437}
+        ry={0.196}
+        transform="rotate(135.188)"
+      />
+      <rect
+        style={{
+          fill: "none",
+          stroke: "#000",
+          strokeWidth: 8.69263,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        width={9.307}
+        height={4.307}
+        x={28.892}
+        y={-138.437}
+        ry={0.196}
+        transform="rotate(135.188)"
+      />
+    </g>
+  </Svg>
+)
+
+export default SvgSettingsIcon
---
+++ GF1-App/Components/SVGShoppingListComponent.js
@@ -0,0 +1,134 @@
+import * as React from "react"
+
+const SvgShoppingListIcon = (props) => (
+  <Svg
+    width="120mm"
+    height="80mm"
+    viewBox="0 0 120 80"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g transform="translate(-27.56 -142.012)">
+      <ellipse
+        style={{
+          fill: "#fff",
+          fillOpacity: 1,
+          stroke: "#000",
+          strokeWidth: 4.82326,
+          strokeDasharray: "none",
+        }}
+        cx={130.534}
+        cy={208.852}
+        rx={5.829}
+        ry={6.296}
+      />
+      <ellipse
+        style={{
+          fill: "#fff",
+          fillOpacity: 1,
+          stroke: "#000",
+          strokeWidth: 4.82326,
+          strokeDasharray: "none",
+        }}
+        cx={79.481}
+        cy={208.852}
+        rx={5.829}
+        ry={6.296}
+      />
+      <Path
+        style={{
+          fill: "#fff",
+          fillOpacity: 1,
+          stroke: "#000",
+          strokeWidth: 4.82325,
+          strokeDasharray: "none",
+        }}
+        d="M129.956 202.556H79.208"
+      />
+      <Path
+        style={{
+          fill: "#fff",
+          fillOpacity: 1,
+          stroke: "#000",
+          strokeWidth: 4.7317,
+          strokeLinecap: "round",
+          strokeDasharray: "none",
+        }}
+        d="m77.88 202.907-31.457-58.444"
+      />
+      <Path
+        style={{
+          fill: "#fff",
+          fillOpacity: 1,
+          stroke: "#000",
+          strokeWidth: 4.80498,
+          strokeLinecap: "round",
+          strokeDasharray: "none",
+        }}
+        d="M54.158 156.939h89.499"
+      />
+      <Path
+        style={{
+          fill: "#fff",
+          fillOpacity: 1,
+          stroke: "#000",
+          strokeWidth: 4.76732,
+          strokeLinecap: "round",
+          strokeLinejoin: "round",
+          strokeDasharray: "none",
+        }}
+        d="M46.29 144.396H29.945"
+      />
+      <Path
+        style={{
+          fill: "#fff",
+          fillOpacity: 1,
+          stroke: "#000",
+          strokeWidth: 4.77666,
+          strokeLinecap: "round",
+          strokeLinejoin: "round",
+          strokeDasharray: "none",
+        }}
+        d="M70.417 188.046h56.499"
+      />
+      <Path
+        style={{
+          fill: "#fff",
+          fillOpacity: 1,
+          stroke: "#000",
+          strokeWidth: 4.81142,
+          strokeLinecap: "round",
+          strokeLinejoin: "round",
+          strokeDasharray: "none",
+        }}
+        d="m143.587 157.271-16.006 30.73"
+      />
+      <Path
+        style={{
+          fill: "#fff",
+          fillOpacity: 1,
+          stroke: "#000",
+          strokeWidth: 2.99454,
+          strokeLinecap: "round",
+          strokeLinejoin: "round",
+          strokeDasharray: "none",
+        }}
+        d="M81.38 168.218h32.006"
+      />
+      <Path
+        style={{
+          fill: "#fff",
+          fillOpacity: 1,
+          stroke: "#000",
+          strokeWidth: 2.81565,
+          strokeLinecap: "round",
+          strokeLinejoin: "round",
+          strokeDasharray: "none",
+        }}
+        d="M83.908 178.143h27.357"
+      />
+    </g>
+  </Svg>
+)
+
+export default SvgShoppingListIcon
